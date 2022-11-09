fa82bbbd1f1a4c2067bb60abed35b740ad64b901
Brendan Byrne <brendan.c.byrne@gmail.com>
2022-11-09 02:25:44 -0500

    Day 4: create parser

---
+++ day_4/src/main.rs
@@ -0,0 +1,59 @@
+use std::fs::read_to_string;
+use std::str::FromStr;
+
+struct Contents {
+    numbers: Vec<i32>,
+    boards: Vec<[[i32; 5]; 5]>,
+}
+
+#[derive(Debug)]
+struct ParseContentsError {}
+
+impl FromStr for Contents {
+    type Err = ParseContentsError;
+
+    fn from_str(s: &str) -> Result<Contents, Self::Err> {
+        let mut iter = s.lines();
+        let mut contents = Self {
+            numbers: iter
+                .next()
+                .unwrap()
+                .split(',')
+                .map(|s| s.parse::<i32>().unwrap())
+                .collect(),
+            boards: Vec::<[[i32; 5]; 5]>::new(),
+        };
+
+        while let Some(mut line) = iter.next() {
+            if line.is_empty() {
+                continue;
+            }
+
+            let mut board = [[0; 5]; 5];
+
+            for (j, v_str) in line.split_whitespace().enumerate() {
+                board[0][j] = v_str.parse::<i32>().unwrap();
+            }
+
+            // Start at 1 because first .unwrap() is performed by the while loop
+            for i in 1..5 {
+                line = iter.next().unwrap();
+                for (j, v_str) in line.split_whitespace().enumerate() {
+                    board[i][j] = v_str.parse::<i32>().unwrap();
+                }
+            }
+
+            contents.boards.push(board);
+        }
+        Ok(contents)
+    }
+}
+
+fn main() {
+    let filename = "/home/bbyrne/projects/advent_of_code_rust/day_4/resources/test_input.txt";
+
+    let Contents { numbers, boards } = match read_to_string(filename) {
+        Ok(string) => Contents::from_str(&string).unwrap(),
+        _ => return,
+    };
+}
